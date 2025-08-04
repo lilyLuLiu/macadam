d54977bb89302aca830877c2ce460aa675102da9
lstocchi <lstocchi@redhat.com>
2025-08-04 15:47:09 +0200

    optimize image copy on windows
    this commit adds an optimization of the image copy process on Windows.
    It leverages robocopy, when available, which is a file replication tool
    built into Windows. By using it we reduce the time to copy of ~40%.
    
    Robocopy is launched with flags:
    - /J: Copies using unbuffered I/O (recommended for large files)
    - /MT: Creates multi-threaded copies with n threads. Default value of n
      is 8
    - /R:<n> Specifies the number of retries on failed copies. 0 in this
      patch
    - /IS Includes Same files, which forces an overwrite even if the destination file appears identical to the source.
    
    Robocopy also uses specific exit code numbers which does not follow
    classic values. E.g. exit code 1 means all copies has been completed
    successfully. All exit codes are listed at https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy#exit-return-codes
    
    If robocopy is not found on the Win system, it fallback to the old copyFile function.
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>

--- pkg/imagepullers/noop.go
+++ pkg/imagepullers/noop.go
@@ -2,10 +2,14 @@ package imagepullers
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
 	"path/filepath"
+
+	"github.com/sirupsen/logrus"
 
 	"github.com/containers/podman/v5/pkg/machine/define"
 
@@ -91,21 +95,17 @@ func doCopyFile(src, dest string, vmType define.VMType) error {
 	}
 	defer srcF.Close()
 
-	destF, err := os.Create(dest)
-	if err != nil {
-		return err
-	}
-	defer destF.Close()
-
 	switch vmType {
 	case define.AppleHvVirt, define.LibKrun:
-		return copyFileMac(srcF, destF)
+		return copyFileMac(srcF, dest)
+	case define.WSLVirt, define.HyperVVirt:
+		return copyFileWin(srcF, dest)
 	default:
-		return copyFile(srcF, destF)
+		return copyFile(srcF, dest)
 	}
 }
 
-func copyFileMac(src, dest *os.File) error {
+func copyFileMac(src *os.File, dest string) error {
 	srcImg, err := qcow2reader.Open(src)
 	if err != nil {
 		return err
@@ -124,18 +124,83 @@ func copyFileMac(src, dest *os.File) error {
 	}
 }
 
-func convertToRaw(srcImg image.Image, dest *os.File) error {
+func copyFileWin(srcF *os.File, dest string) error {
+	binary, err := exec.LookPath("robocopy")
+	if err != nil {
+		logrus.Debugf("warning: failed to get robocopy binary: %v. Falling back to default file copy between %s and %s\n", err, srcF.Name(), dest)
+		return copyFile(srcF, dest)
+	}
+
+	srcDir, srcFile := filepath.Split(srcF.Name())
+	destDir := filepath.Dir(dest)
+
+	// Executes the robocopy command with options optimized for a fast, single-file copy.
+	// /J:   Copies using unbuffered I/O (better for large files).
+	// /MT:  Enables multi-threaded copying for improved performance.
+	// /R:0: Sets retries on failed copies to 0 to avoid long waits.
+	// /IS:  Includes Same files, which forces an overwrite even if the destination
+	//       file appears identical to the source.
+	cmd := exec.Command(binary, "/J", "/MT", "/R:0", "/IS", srcDir, destDir, srcFile)
+	if logrus.IsLevelEnabled(logrus.DebugLevel) {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+	} else {
+		cmd.Stdout = io.Discard
+		cmd.Stderr = io.Discard
+	}
+
+	err = cmd.Run()
+	if err != nil {
+		// robocopy does not use classic exit codes like linux commands, so we need to check for specific errors
+		// Only exit code 8 is considered an error, all other exit codes are considered successful with exceptions
+		// see https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			exitCode := exitErr.ExitCode()
+			if exitCode >= 8 {
+				return fmt.Errorf("failed to run robocopy: %w", err)
+			}
+		} else {
+			return fmt.Errorf("failed to run robocopy: %w", err)
+		}
+	}
+
+	if err := os.Remove(dest); err != nil && !errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("failed to remove existing destination file: %w", err)
+	}
+
+	err = os.Rename(filepath.Join(destDir, srcFile), dest)
+	if err != nil {
+		return fmt.Errorf("failed to rename file: %w", err)
+	}
+
+	return nil
+}
+
+func convertToRaw(srcImg image.Image, dest string) error {
+	destF, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer destF.Close()
+
 	if err := srcImg.Readable(); err != nil {
 		return fmt.Errorf("source image is not readable: %w", err)
 	}
 
-	return convert.Convert(dest, srcImg, convert.Options{})
+	return convert.Convert(destF, srcImg, convert.Options{})
 }
 
-func copyFile(src, dst *os.File) error {
-	bufferedWriter := bufio.NewWriter(dst)
+func copyFile(src *os.File, dest string) error {
+	destF, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer destF.Close()
+
+	bufferedWriter := bufio.NewWriter(destF)
 	defer bufferedWriter.Flush()
 
-	_, err := io.Copy(bufferedWriter, src)
+	_, err = io.Copy(bufferedWriter, src)
 	return err
 }
